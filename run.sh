28e933fc34c7db86616e66e391b79f92ee2dbc18
01alex <xanomcr@gmail.com>
2021-03-02 14:38:25 +0000

    update gunicorn script with logs

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ export FLASK_CONFIG='production'
 # Change secret key before launching in production!
 export SECRET_KEY='1209876'
 
-gunicorn -b 0.0.0.0:5000 run:app --timeout 180 --keep-alive 10000 --limit-request-line 40940 --workers 8
+gunicorn -b 0.0.0.0:5000 run:app --timeout 180 --keep-alive 10000 --limit-request-line 40940 --workers 8 --log-level debug --access-logfile access.log --error-logfile error.log
