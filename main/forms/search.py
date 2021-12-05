40b8c235d1600ef1b8fc8dfb59e2c5359bd6061d
01alex <xanomcr@gmail.com>
2021-12-05 03:35:45 +0000

    update requirements

--- main/forms/search.py
+++ main/forms/search.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, HiddenField
+from wtforms import StringField, HiddenField
 from flask import Blueprint
 
 search_form = Blueprint('SearchForm', __name__)
 
+
 class SearchForm(FlaskForm):
-    query = TextField('Search', [])
-    date_from = TextField('from', [])
-    date_to = TextField('to', [])
-    last_years = TextField('last_years', [])
-    news_sources = TextField('news_sources', [])
-    lang = HiddenField('lang',[],default="pt")
+    query = StringField('Search', [])
+    date_from = StringField('from', [])
+    date_to = StringField('to', [])
+    last_years = StringField('last_years', [])
+    news_sources = StringField('news_sources', [])
+    lang = HiddenField('lang', [], default="pt")
